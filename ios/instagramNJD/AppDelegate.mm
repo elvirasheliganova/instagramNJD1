e58a231663b2f60c929bcd286b1ec346696eecb9
Elvira Shelyganova <elvirashelyganova@gmail>
2023-04-14 14:21:46 +0200

    deep link for ios

--- ios/instagramNJD/AppDelegate.mm
+++ ios/instagramNJD/AppDelegate.mm
@@ -1,3 +1,4 @@
+#import <React/RCTLinkingManager.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -15,6 +16,11 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
+
+
+
+
+
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
@@ -129,5 +135,19 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 }
 
 #endif
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 
+// Add this inside `@implementation AppDelegate` above `@end`:
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
 @end
